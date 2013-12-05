7422e40cb13fae39d69ff6c1c93fde319868394d
fzerorubigd <fzerorubigd@gmail.com>
2013-12-05 21:40:02 +0330

    add more test to fail

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,11 +7,15 @@ class ExampleTest extends PHPUnit_Framework_TestCase
     {
         $obj = new \Cybits\Example();
         $this->assertEquals(10, $obj->min(10, 100));
+        $this->assertEquals(5, $obj->min(10, 5));
+        $this->assertEquals(1, $obj->min(1, 100));
     }
 
     public function testMax()
     {
         $obj = new \Cybits\Example();
         $this->assertEquals(100, $obj->max(10, 100));
+        $this->assertEquals(70, $obj->max(11, 70));
+        $this->assertEquals(20, $obj->max(10, 20));
     }
 }
